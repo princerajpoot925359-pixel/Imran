b6913f07156485e1d48d11c6d822bd10ab74aa8b
princerajpoot925359-pixel <princerajpoot925359@gmail.com>
2026-02-05 20:42:04 +0530

    Update help2.js

--- Priyansh/commands/help2.js
+++ Priyansh/commands/help2.js
@@ -67,7 +67,7 @@ module.exports. run = function({ api, event, args, getText }) {
     for (let item of returnArray) msg += `「 ${++i} 」${prefix}${item}\n`;
     
     
-    const siu = `Command list 📄\nMade by ◉❖जुल्मी≛जाट❖◉ 🥀\nFor More Information type /help (command name) ✨`;
+    const siu = `Command list 📄\nMade by ◉❖KHAN≛SAHAB❖◉ 🥀\nFor More Information type /help (command name) ✨`;
     
  const text = `\nPage (${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)})`;
  
